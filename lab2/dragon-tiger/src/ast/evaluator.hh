f6a83ef7b05146c7c88a7d90787303ff15129a4e
D.Burchenko <burch1302@gmail.com>
2025-05-31 16:05:22 +0300

    Added ast::Evaluator

---
+++ lab2/dragon-tiger/src/ast/evaluator.hh
@@ -0,0 +1,115 @@
+#pragma once
+
+#include "nodes.hh"
+#include "../utils/errors.hh"
+
+namespace ast {
+
+    class Evaluator : public ConstASTIntVisitor {
+    public:
+        // IntegerLiteral Ч просто вернуть value
+        virtual int32_t visit(const IntegerLiteral& node) override {
+            return node.value;
+        }
+
+        // BinaryOperator Ч вычислить левый и правый, выполнить операцию
+        virtual int32_t visit(const BinaryOperator& node) override {
+            int32_t left = node.get_left().accept(*this);
+            int32_t right = node.get_right().accept(*this);
+
+            switch (node.op) {
+            case o_plus: return left + right;
+            case o_minus: return left - right;
+            case o_times: return left * right;
+            case o_divide:
+                if (right == 0) utils::error(node.loc, "Division by zero");
+                return left / right;
+            case o_eq: return left == right ? 1 : 0;
+            case o_neq: return left != right ? 1 : 0;
+            case o_lt: return left < right ? 1 : 0;
+            case o_le: return left <= right ? 1 : 0;
+            case o_gt: return left > right ? 1 : 0;
+            case o_ge: return left >= right ? 1 : 0;
+            default:
+                utils::error(node.loc, "Unknown binary operator");
+            }
+            return 0; // „тобы избежать предупреждений
+        }
+
+        // Sequence Ч вычислить все, вернуть последний результат
+        virtual int32_t visit(const Sequence& node) override {
+            const auto& exprs = node.get_exprs();
+            if (exprs.empty()) {
+                utils::error(node.loc, "Empty sequence");
+            }
+            int32_t val = 0;
+            for (auto expr : exprs) {
+                val = expr->accept(*this);
+            }
+            return val;
+        }
+
+        // IfThenElse Ч вычислить условие, потом нужную ветку
+        virtual int32_t visit(const IfThenElse& node) override {
+            int32_t cond = node.get_condition().accept(*this);
+            if (cond != 0) {
+                return node.get_then_part().accept(*this);
+            }
+            else {
+                return node.get_else_part().accept(*this);
+            }
+        }
+
+        // ƒл€ всех остальных Ч ошибка
+        virtual int32_t visit(const StringLiteral& node) override {
+            utils::error(node.loc, "StringLiteral evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const Let& node) override {
+            utils::error(node.loc, "Let evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const Identifier& node) override {
+            utils::error(node.loc, "Identifier evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const VarDecl& node) override {
+            utils::error(node.loc, "VarDecl evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const FunDecl& node) override {
+            utils::error(node.loc, "FunDecl evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const FunCall& node) override {
+            utils::error(node.loc, "FunCall evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const WhileLoop& node) override {
+            utils::error(node.loc, "WhileLoop evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const ForLoop& node) override {
+            utils::error(node.loc, "ForLoop evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const Break& node) override {
+            utils::error(node.loc, "Break evaluation not supported");
+            return 0;
+        }
+
+        virtual int32_t visit(const Assign& node) override {
+            utils::error(node.loc, "Assign evaluation not supported");
+            return 0;
+        }
+    };
+
+} // namespace ast
--- lab2/dragon-tiger/src/driver/driver.cc
+++ lab2/dragon-tiger/src/driver/driver.cc
@@ -4,6 +4,7 @@
 #include "../ast/ast_dumper.hh"
 #include "../parser/parser_driver.hh"
 #include "../utils/errors.hh"
+#include "../ast/evaluator.hh"
 
 int main(int argc, char **argv) {
   std::vector<std::string> input_files;
@@ -12,6 +13,7 @@ int main(int argc, char **argv) {
   options.add_options()
   ("help,h", "describe arguments")
   ("dump-ast", "dump the parsed AST")
+  ("eval", "evaluate the AST and print the result")
   ("trace-parser", "enable parser traces")
   ("trace-lexer", "enable lexer traces")
   ("verbose,v", "be verbose")
@@ -27,6 +29,9 @@ int main(int argc, char **argv) {
                 .run(),
             vm);
   po::notify(vm);
+
+  bool eval_flag = vm.count("eval") > 0;
+  bool dump_ast_flag = vm.count("dump-ast") > 0;
 
   if (vm.count("help")) {
     std::cout << options << "\n";
@@ -47,6 +52,16 @@ int main(int argc, char **argv) {
     ast::ASTDumper dumper(&std::cout, vm.count("verbose") > 0);
     parser_driver.result_ast->accept(dumper);
     dumper.nl();
+  }
+
+    if (eval_flag && dump_ast_flag) {
+    utils::error("Cannot use both --eval and --dump-ast");
+  }
+
+  if (eval_flag) {
+    ast::Evaluator evaluator;
+    int value = parser_driver.result_ast->accept(evaluator);
+    std::cout << value << std::endl;
   }
   delete parser_driver.result_ast;
   return 0;
